002249fd0ae5de0f25c0ac85a637e62a034c201a
Gitter <abhishekbhujang@choco.la>
2020-02-01 16:18:46 +0530

    Add files via upload

---
+++ STSNO.C
@@ -0,0 +1,27 @@
+#include<iostream.h>
+#include<conio.h>
+#include<iomanip.h>
+void main()
+{
+int a;
+clrscr();
+cout<<"ENTER ROLL NO ";
+cin>>a;
+if (a==1)
+cout<<"NAME= ABHISHEK"<<endl<<"STS NO= 132476567869";
+else if (a==2)
+cout<<"NAME= ANIRUDH"<<endl<<"STS NO= 987654321";
+else if (a==3)
+cout<<"NAME= QWERTY"<<endl<<"STS NO= 1345689796";
+else if (a==4)
+cout<<"NAME= TREWQ"<<endl<<"STS NO= 8676452457";
+else if (a==5)
+cout<<"NAME= VGURHFTU"<<endl<<"STS NO= 789685674";
+else if (a==6)
+cout<<"NAME= GFUIRTUG"<<endl<<"STS NO= 7543535486";
+else if (a==7)
+cout<<"NAME= JTIUGKJG7U"<<endl<<"STS NO= 8563563265";
+else
+cout<<"STUDENT NOT FOUND ";
+getch();
+5}
